031ca59307db9907585b5cfad09ba55dbefba1bf
tobias <tobiaslehner@me.com>
2019-12-19 16:57:13 +0100

    Problem 187
    Still too slow

--- Problem_187.py
+++ Problem_187.py
@@ -8,18 +8,59 @@ not necessarily distinct, prime factors:
 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
 
 How many composite integers, n < 10^8, have precisely two,
-not necessarily distinct, prime factors?
-"""
+not necessarily distinct, prime factors?"""
 
-max_num = 30
-print("starting")
+import time
 
-for number in range(2, max_num + 1):
-    print(number)
+arr = [0] * 1000
+
+t = time.time()
+max_num = 100000
+print("PROBLEM 187")
+print("calculating...")
+total_two_composites = 0
+set_of_tested_nums = set()
+
+number = 2
+while True:
+    finished = False
+    continueFlag = True
+    orig_number = number
+    prime_factors = 0
     divisor = 2
-    while number != 1:
-        if number % divisor == 0:
-            number = number / divisor
-            print(number)
-        divisor += 1
+    # print(f"Zerlegung von {orig_number}")
+    while continueFlag:
+        if number % divisor != 0:
+            if divisor == 2:
+                divisor += 1
+            else:
+                divisor += 2
+        else:
+            number = int(number / divisor)
+            prime_factors += 1
+            if (prime_factors == 2 and number != 1) or (
+                number in set_of_tested_nums
+            ):
+                continueFlag = False
+                finished = False
 
+            if number == 1:
+                finished = True
+                continueFlag = False
+
+    if finished and prime_factors == 2:
+        # print(f"{orig_number} is a composite integer")
+        total_two_composites += 1
+    if prime_factors > 1:
+        set_of_tested_nums.add(orig_number)
+    number = orig_number + 1
+    if number == max_num:
+        break
+
+
+print(
+    f"There are {total_two_composites} composites containing precisely two prime factors."
+)
+
+elapsed = time.time() - t
+print(f"{elapsed} seconds")
