35da53c9fd42514fd598165fca68c87813ac7f88
tobias <tobiaslehner@me.com>
2019-12-19 14:23:34 +0100

    Initial Commit

---
+++ Problem_187.py
@@ -0,0 +1,25 @@
+""" Problem 187: Semiprimes
+
+A composite is a number containing at least two prime factors.
+For example, 15 = 3 × 5; 9 = 3 × 3; 12 = 2 × 2 × 3.
+
+There are ten composites below thirty containing precisely two,
+not necessarily distinct, prime factors:
+4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
+
+How many composite integers, n < 10^8, have precisely two,
+not necessarily distinct, prime factors?
+"""
+
+max_num = 30
+print("starting")
+
+for number in range(2, max_num + 1):
+    print(number)
+    divisor = 2
+    while number != 1:
+        if number % divisor == 0:
+            number = number / divisor
+            print(number)
+        divisor += 1
+
